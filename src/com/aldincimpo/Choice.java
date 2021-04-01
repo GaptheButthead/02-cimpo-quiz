f7eb620c0c638633b0652865a1ee913a92455121
Aldin Cimpo <aldin.cimpo@hotmail.com>
2021-04-01 09:49:33 +0200

    Made 2 Classes

--- src/com/aldincimpo/Choice.java
+++ src/com/aldincimpo/Choice.java
@@ -1,4 +1,11 @@
 package com.aldincimpo;
 
 public class Choice {
+    public String txt;
+    public boolean ok;
+
+    public Choice(String txt, boolean ok) {
+        this.txt = txt;
+        this.ok = ok;
+    }
 }
--- src/com/aldincimpo/Question.java
+++ src/com/aldincimpo/Question.java
@@ -1,5 +1,11 @@
 package com.aldincimpo;
 
+import java.util.ArrayList;
+
 public class Question {
+
+
+    public String txt;
+    public ArrayList<Choice> choices;
 
 }
